d7dbccd30c36980ee3a6f05e28da5533bb868140
Krzysztof Romanowski <romanowski.kr@gmail.com>
2015-04-15 11:09:55 +0200

    Update MissingLineNumbers.scala

--- src/main/scala/tests/MissingLineNumbers.scala
+++ src/main/scala/tests/MissingLineNumbers.scala
@@ -3,5 +3,4 @@ package tests
 object MissingLineNumbers extends App{
   val foo: Class[_] = classOf //error without position, line or file
   val foo1: Class[_] = classOf[X] //good error, all info contained
-
 }
